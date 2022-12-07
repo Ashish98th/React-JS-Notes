0735547459f13b9f32f455c980317ca4a749dbb7
Ashish Nath <107504126+Ashish98th@users.noreply.github.com>
2022-12-07 14:56:28 +0530

    file uploaded in Context Api folder

---
+++ Context Api/Context API/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Dish from "./Dish";
+import Home from "./Home";
+function App(){
+return(
+   <>
+   <div>
+  <Home/>
+  </div>
+    
+  </>
+)
+
+  
+}
+export default App;
+
+
