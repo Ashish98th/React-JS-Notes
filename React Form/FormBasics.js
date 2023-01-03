c0258ea5bf2d44c04ddd6c1d1509b9e5124e2f17
Ashish Nath <107504126+Ashish98th@users.noreply.github.com>
2023-01-03 15:04:58 +0530

    update FormBasic

--- React Form/FormBasics.js
+++ React Form/FormBasics.js
@@ -1,66 +1,57 @@
-import React,{useState} from "react";
+import React,{useState,useRef} from "react";
 import "./App.css";
 const FormBasics=()=>{
-    const[firstName,setFirstName]=useState("");
+
+    const[Firstname,setFirstname]=useState("");
     const[LastName,setLastName]=useState("");
-    const[email,setemail]=useState("");
+    const[formArray,setFormArray]=useState([]);
+    console.log(formArray);
 
-    const[formArray,setformArray]=useState([]);
+    const container=useRef();
+    console.log(container.current)
 
-    const handleSubmit=(e)=>{
-    e.preventDefault();
-
-    const users={fName:firstName,lName:LastName,emailId:email};
-
- if(firstName && LastName && email){
-    setformArray((oldValue)=>{
-        return [...oldValue,users] //putting the users inside formArray
-    })
- }else{
-    console.log("Should not be Blank")
- }
+    const backgroundChange=()=>{
+       container.current.style.backgroundColor="blue";
     }
-    console.log(formArray); //everything will be inside formArray
-    
-    return(
+    function submitHandler(e){
+        e.preventDefault();
+
+        const users=[{fname:Firstname,lname:LastName}]
+
+        if(Firstname && LastName){
+            setFormArray((oldvalue)=>{
+                return [...oldvalue,users]
+            })
+            setFirstname("")
+            setLastName("")
+            
+        }else{
+            console.log("Shouldn't be Blank")
+        }
+      
+    }
+  
+    return (
         <div>
-            <h1 className="heading">React Form Basics</h1>
-            <hr></hr>
+<form onSubmit={submitHandler}>
+    <div>
+    <label htmlFor="Firstname" >First Name:</label>
+    <input value={Firstname} onChange={(e)=>setFirstname(e.target.value)} type="text" name="Firstname" id="firtsname" />
+    </div>
 
-<form onSubmit={handleSubmit}>
+    <div>
+    <label htmlFor="Lastname" >Last Name:</label>
+    <input value={LastName} onChange={(e)=>setLastName(e.target.value)} type="text" name="Firstname" id="LastName" />
+    </div>
 
-<div className="container">
-
-<div className="firstName">
-    <label htmlFor="first__name">First Name:</label>
-    <input value={firstName} onChange={(e)=>setFirstName(e.target.value)} type="text" name="first_name" id="first__name"/>
-</div>
-
-
-<div className="LastName">
-    <label htmlFor="last__name">Last Name:</label>
-    <input value={LastName} onChange={(e)=>setLastName(e.target.value)} type="text" name="last_name" id="last__name"/>
-</div>
-
-<div className="email">
-    <label htmlFor="email">email:</label>
-    <input value={email} onChange={(e)=>setemail(e.target.value)} type="text" name="email" id="email"/>
-</div>
-
-<div className="submit">
+<div>
     <button type="submit">Submit</button>
 </div>
-</div>
+</form>
 
-
-    </form>
+<div ref={container}>My Div</div>
+<button type="button" onClick={backgroundChange}>Change Background</button>
         </div>
     )
 }
 export default FormBasics;
-
-
-//When type value onChange event will trigger
-// e.target.value event object takes event.target.value means the typed value will be set inside state & it's going in firstName
-
-//Whrn You typed some value the value will be set inside the state 
